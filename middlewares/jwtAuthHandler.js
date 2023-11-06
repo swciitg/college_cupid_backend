eedad50ed048667575ea959b262dd30d6b44fa18
Hardik Roongta <hardikroongta8@gmail.com>
2023-11-07 01:01:25 +0530

    minor change

--- middlewares/jwtAuthHandler.js
+++ middlewares/jwtAuthHandler.js
@@ -5,20 +5,20 @@ const {RequestValidationError} = require('../errors/requestValidationError');
 exports.authenticateToken = (req, res, next) => {
     const authorization = req.headers['authorization'];
     if(!authorization) {
-        throw new RequestValidationError('Access Token is not passed');
+        return next(new RequestValidationError('Access Token is not passed'));
     }
 
     const token = authorization.split(' ')[1];
 
     if(token == null) {
-        throw new RequestValidationError('Access Token is not passed');
+        return next(new RequestValidationError('Access Token is not passed'));
     }
 
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,  (err, user) => {
         if(err) {
             console.log(err.message);
-            throw new AccessTokenError(err.message);
+            return next(new AccessTokenError(err.message));
         }
         console.log(user);
         req.email = user.email;
