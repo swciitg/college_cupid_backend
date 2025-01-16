bfb5965567a8d69979b586668a575b3e374f0a0e
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-01-16 20:17:59 +0530

    Updated UserProfile schema

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,58 +1,91 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 const userProfileSchema = new Schema({
-    'name': {
-        type: String,
-        required: [true, 'Name is a required field!']
+  name: {
+    type: String,
+    required: [true, "Name is a required field!"],
+  },
+  gender: {
+    type: String,
+    required: [true, "Gender is a required field!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is a required field!"],
+    unique: true,
+  },
+  sexualOrientation: {
+    type: {
+      type: String,
+      default: "",
     },
-    'gender': {
-        type: String,
-        required: [true, 'Gender is a required field!']
+    display: {
+      type: Boolean,
+      default: false,
     },
-    'email': {
-        type: String,
-        required: [true, 'Email is a required field!'],
-        unique: true
+  },
+  'profilePicUrl': {
+    type: String,
+    default: ''
+},
+  program: {
+    type: String,
+    required: [true, "Program is a required field!"],
+  },
+  yearOfJoin: {
+    type: Number,
+    required: [true, "Year of join is a required field!"],
+  },
+  interests: {
+    type: [String],
+    default: [],
+    validate: [
+      interestArrayLimit,
+      "Interests must lie in the range of 5 to 20!",
+    ],
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  datingPreference: {
+    gender: {
+      type: String,
     },
-    'profilePicUrl': {
-        type: String,
-        default: ''
+    course: {
+      type: String,
     },
-    'program': {
-        type: String,
-        required: [true, 'Program is a required field!']
+    year: {
+      type: String,
     },
-    'yearOfJoin': {
-        type: Number,
-        required: [true, 'Year of join is a required field!']
+  },
+  relationshipGoals: {
+    goal: {
+      type: String,
+      default: "",
     },
-    'interests': {
-        type: [String],
-        default: [],
-        validate: [interestArrayLimit, 'Interests must lie in the range of 5 to 20!']
+    display: {
+      type: Boolean,
+      default: false,
     },
-    'bio': {
-        type: String,
-        default: ''
-    },
-    'publicKey': {
-        type: String,
-        required: [true, 'Public Key is a required field!'],
-    },
-    shuffleOrder: {
-        type: [Number],
-        default: undefined,
-    },
-    lastShuffle: {
-        type: Number,
-        default: undefined,
-    }
+  },
+  publicKey: {
+    type: String,
+    required: [true, "Public Key is a required field!"],
+  },
+  shuffleOrder: {
+    type: [Number],
+    default: undefined,
+  },
+  lastShuffle: {
+    type: Number,
+    default: undefined,
+  },
 });
-
 function interestArrayLimit(val) {
-    console.log("i was called");
-    console.log(val);
-    return val.length <= 20 && val.length >= 5;
-  }
+  console.log("i was called");
+  console.log(val);
+  return val.length <= 20 && val.length >= 5;
+}
 
-const UserProfile = model('UserProfile', userProfileSchema);
+const UserProfile = model("UserProfile", userProfileSchema);
 module.exports = UserProfile;
