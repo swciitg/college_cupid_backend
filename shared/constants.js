a04b6441d63ae97bcf0b85dbf16af10da28117e3
kvineet002 <134228421+kvineet002@users.noreply.github.com>
2025-01-27 01:52:18 +0530

    empty commit

--- shared/constants.js
+++ shared/constants.js
@@ -3,6 +3,6 @@ exports.AdminList = ["r.hardik@iitg.ac.in", "swc@iitg.ac.in","vineet.mech22@iitg
 exports.GuestUserInfo = {
     rollNumber: "200000000",
     displayName: "GUEST USER"
-};
+}; 
 
 exports.GuestEmails = ["swc@iitg.ac.in"];
